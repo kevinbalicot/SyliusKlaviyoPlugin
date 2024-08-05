b6cb31c729d4bb659f42af4b5656e5e680b11c01
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-05 12:48:29 +0200

    Add false positives

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,10 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('setono/client-id-bundle', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/http-client', [ErrorType::UNUSED_DEPENDENCY])
 ;
